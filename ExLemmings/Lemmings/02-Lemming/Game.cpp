b567b165138999137f1c57ba34fb557e2d653a70
Ralu <ralucris.v@gmail.com>
2018-04-18 13:00:32 +0200

    Fixed lemming start position

--- ExLemmings/Lemmings/02-Lemming/Game.cpp
+++ ExLemmings/Lemmings/02-Lemming/Game.cpp
@@ -27,7 +27,8 @@ bool Game::update(int deltaTime)
 			menu.init(escMenuBackground, escMenuButtonSprite, escMenuButtonsPos);
 			sceneActive = true;
 			sceneVisible = true;
-			scene.init(sceneMaps[0], sceneMasks[0], glm::vec2(60, 30), glm::vec2(360, 130), glm::vec2(60, 30), glm::vec2(512, 159));
+			//                                      entry              exit               lemmings           drawing size
+			scene.init(sceneMaps[0], sceneMasks[0], glm::vec2(60, 30), glm::vec2(360, 130), glm::vec2(180, 30), glm::vec2(512, 159));
 			break;
 		default:
 			break;
@@ -41,7 +42,8 @@ bool Game::update(int deltaTime)
 
 	if (sceneActive) {
 		if (scene.checkFinished())
-			scene.init(sceneMaps[0], sceneMasks[0], glm::vec2(60, 30), glm::vec2(240, 131), glm::vec2(60, 30), glm::vec2(512, 159));
+			//                                      entry              exit               lemmings           drawing size
+			scene.init(sceneMaps[0], sceneMasks[0], glm::vec2(60, 30), glm::vec2(240, 131), glm::vec2(180, 30), glm::vec2(512, 159));
 		else
 			scene.update(deltaTime);
 	}
@@ -64,10 +66,10 @@ void Game::keyPressed(int key)
 	if(key == 27) // Escape code
 		bPlay = false;
 	if (key == 49) //1
-		scene.init(sceneMaps[0], sceneMasks[0], glm::vec2(60, 30), glm::vec2(60, 30), glm::vec2(180, 30), glm::vec2(512, 159));
+		scene.init(sceneMaps[0], sceneMasks[0], glm::vec2(60, 30), glm::vec2(240, 131), glm::vec2(180, 30), glm::vec2(512, 159));
 		//                                      entry              exit               lemmings           drawing size
 	if (key == 50) //2
-		scene.init(sceneMaps[1], sceneMasks[1], glm::vec2(60, 30), glm::vec2(60, 30), glm::vec2(110, 10), glm::vec2(443, 160));
+		scene.init(sceneMaps[1], sceneMasks[1], glm::vec2(60, 30), glm::vec2(240, 131), glm::vec2(110, 10), glm::vec2(443, 160));
 	keys[key] = true;
 	
 	if (sceneActive)
